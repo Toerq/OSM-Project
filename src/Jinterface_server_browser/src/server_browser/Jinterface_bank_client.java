081dfadead7f50cec64041abcc3e512813b34f88
Niklas Hokenstrom <niklashokenstrom@gmail.com>
2014-04-24 09:28:04 +0200

    Jinterface for add-function added

--- src/Jinterface_server_browser/src/server_browser/Jinterface_bank_client.java
+++ src/Jinterface_server_browser/src/server_browser/Jinterface_bank_client.java
@@ -9,9 +9,33 @@ public class Jinterface_bank_client {
 		this.conn = new ErlConnection("enode", "erlang");
 	}
 	
-	/**
-	 * @param args
-	 */
+	public void add(String servername, int[] ip, int[] destIp) {
+		OtpErlangObject[] argArray = new OtpErlangObject[3];
+		OtpErlangObject sName = new OtpErlangAtom(servername);
+		argArray[0] = sName;
+		
+		int size = ip.length;
+		OtpErlangObject tmp[] = new OtpErlangObject[size];
+		for (int i = 0; i < size; i++) {
+			tmp[i] = new OtpErlangInt(ip[i]);
+		}
+		OtpErlangTuple IpTuple = new OtpErlangTuple(tmp);
+		argArray[1] = IpTuple;
+		
+		size = destIp.length;
+		OtpErlangObject tmp2[] = new OtpErlangObject[size];
+		for (int i = 0; i < size; i++) {
+			tmp2[i] = new OtpErlangInt(destIp[i]);
+		}
+		OtpErlangTuple destIpTuple = new OtpErlangTuple(tmp2);
+		argArray[2] = destIpTuple;
+		
+		OtpErlangList argList = new OtpErlangList(argArray);
+		conn.sendRPC("bank_client", "add", argList);
+		OtpErlangObject received = conn.receiveRPC();
+		System.out.println(received + "\n");
+	}
+	
 	public void available(int[] destIp) {
 		int size = destIp.length;
 		OtpErlangObject tmp[] = new OtpErlangObject[size];
@@ -27,8 +51,10 @@ public class Jinterface_bank_client {
 	
 	public static void main(String[] args) {
 		Jinterface_bank_client client = new Jinterface_bank_client("enode", "erlang");
-		int[] IP = {192,168,1,2};
-		client.available(IP);
+		int[] ip = {192,168,1,2};
+		int[] newServerIp = {1,2,3,4};
+		client.add("hejsan", newServerIp, ip);
+		client.available(ip);
 		client.conn.disconnect();
 	}
 }
