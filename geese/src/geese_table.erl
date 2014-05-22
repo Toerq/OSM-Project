39cdae6fcb5f7ab83a3545a78ad7ef6b10ee58e7
Jonas Nilson <jonas.nilson.8705@student.uu.se>
2014-05-22 16:32:41 +0200

    Db_name

--- geese/src/geese_table.erl
+++ geese/src/geese_table.erl
@@ -39,7 +39,8 @@ checkout(Who, Book) -> gen_server:call(?MODULE, {checkout, Who, Book}).
 
 init([]) ->
     State_sender = spawn(fun() -> game_state:state_sender(game_logic:make_new_state()) end),
-    Db_name = list_to_atom(erlang:ref_to_list(make_ref())),
+    %%Db_name = list_to_atom(erlang:ref_to_list(make_ref())),
+    Db_name = action,
     Tick = ?TICKRATE,
     game_state:start(Db_name, State_sender, Tick),
     {ok, #table_state{number_of_players = 0, max_players = 20, state_sender = State_sender, db_name = Db_name, players = []}}.
